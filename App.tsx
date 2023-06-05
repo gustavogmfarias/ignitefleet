4dc40393b4da6db0c344cd53bb6239d190eb2f41
Gustavo <gustavogmfarias@gmail.com>
2023-06-05 15:47:34 -0300

    .

--- App.tsx
+++ App.tsx
@@ -1,5 +1,34 @@
-import { SignIn } from "./src/screens/SignIn";
+import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import theme from './src/theme';
+
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
-  return <SignIn />;
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  if(!fontsLoaded) {
+    return (
+      <Loading />
+    )
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent" 
+        translucent 
+      />
+      <SignIn />
+    </ThemeProvider>
+  );
 }
+
