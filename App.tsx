34653c8d9a8de5a4850b69fe0420a770a54515c0
Gustavo <gustavogmfarias@gmail.com>
2023-06-27 15:29:19 -0300

    .

--- App.tsx
+++ App.tsx
@@ -1,34 +1,46 @@
-import { ThemeProvider } from 'styled-components/native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { ThemeProvider } from "styled-components/native";
+import { StatusBar } from "react-native";
+import { AppProvider, UserProvider } from "@realm/react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import theme from './src/theme';
+import theme from "./src/theme";
 
-import { SignIn } from './src/screens/SignIn';
-import { Loading } from './src/components/Loading';
-import { StatusBar } from 'react-native';
+import { REALM_APP_ID } from "@env";
+
+import { Routes } from "./src/routes";
+
+import { SignIn } from "./src/screens/SignIn";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
-
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
-  })
+    Roboto_700Bold,
+  });
 
-  if(!fontsLoaded) {
-    return (
-      <Loading />
-    )
+  if (!fontsLoaded) {
+    return <Loading />;
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar 
-        barStyle="light-content" 
-        backgroundColor="transparent" 
-        translucent 
-      />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <SafeAreaProvider>
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <UserProvider fallback={SignIn}>
+            <Routes />
+          </UserProvider>
+        </SafeAreaProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
-
