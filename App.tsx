0a0c5ce977924d5c2dd576560bc3d23eca64b4d3
Gustavo <gustavogmfarias@gmail.com>
2023-06-27 16:21:36 -0300

    .

--- App.tsx
+++ App.tsx
@@ -13,6 +13,7 @@ import theme from "./src/theme";
 import { REALM_APP_ID } from "@env";
 
 import { Routes } from "./src/routes";
+import { RealmProvider } from "./src/libs/realm";
 
 import { SignIn } from "./src/screens/SignIn";
 import { Loading } from "./src/components/Loading";
@@ -37,7 +38,9 @@ export default function App() {
             translucent
           />
           <UserProvider fallback={SignIn}>
-            <Routes />
+            <RealmProvider>
+              <Routes />
+            </RealmProvider>
           </UserProvider>
         </SafeAreaProvider>
       </ThemeProvider>
